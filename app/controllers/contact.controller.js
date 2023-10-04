af1110255c0da39bbd495a27d6ec11ac5e00a3ed
thuylaiha <125327820+thuylaiha@users.noreply.github.com>
2023-10-04 14:37:01 +0700

    Cai dat handler create

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,10 +1,50 @@
+const ApiError = require("../api-error");
+const ContactService = require("../services/contact.service");
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+
 exports.create = (req, res) => {
     res.send({ message: "create handler"});
+}
+//Create and save a new contact
+exports.create = async (req,res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Name can not be empty"));
+    }
+
+    try {
+        const ContactService = new ContactService(Mongodb.client);
+        const document = await ContactService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        );
+    }
 };
 
 exports.findAll = (req, res) => {
     res.send({ message: "findAll handler"});
 };
+// Retrieve all contacts of a user from the database
+exports.findAll = async (req,res,next) => {
+    let documents = [];
+
+    try{
+        const ContactService = new ContactService(MongoDB.client);
+        const {name } = req.query;
+        if(name) {
+            documents = await ContactService.findByName(name);
+        } else {
+            documents = await ContactService.find({});
+        }
+    } catch (error) {
+        return next(
+            new ApiError(400, "An error occurred while retrieving contacts")
+        );
+    }
+    return res.send(documents);
+}
 
 exports.findOne = (req, res) => {
     res.send({ message: "findOne handler"});
